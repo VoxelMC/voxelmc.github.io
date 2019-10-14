dfcbdfe8031387645fa0953fe851a941ca9cf8ed
Voxel <voxelmc@hotmail.com>
2019-10-14 14:27:53 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-import { parse } from "querystring";
-
 const letters = [" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 document.getElementById("genButton").onclick = () => {
     const p = document.getElementById("p").value === 0 ? 0 : document.getElementById("p").value;
